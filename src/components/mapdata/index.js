55de02e7fe2d41ea508d6e9ae65585891f88b1b0
djy1028 <405857063@qq.com>
2021-06-15 18:19:48 +0800

    调试英文版和移动端自适应

--- src/components/mapdata/index.js
+++ src/components/mapdata/index.js
@@ -181,87 +181,85 @@ var series = [];
     ["上海", BJData]
 ].forEach(function(item, i) {
     series.push({
-            
-            type: "effectScatter",
-            coordinateSystem: "geo",
-            zlevel: 2,
-            rippleEffect: {
-                //涟漪特效
-                period: 4, //动画时间，值越小速度越快
-                brushType: "stroke", //波纹绘制方式 stroke, fill
-                scale: 4 //波纹圆环最大限制，值越大波纹越大
-            },
-            label: {
-                normal: {
-                    show: true,
-                    position: "right", //显示位置
-                    offset: [5, 0], //偏移设置
-                    formatter: "{b}" //圆环显示文字
-                },
-                emphasis: {
-                    show: true,
-                    color: "#FF6A6A"
-                }
-            },
-            symbol: "circle",
-            symbolSize: function(val) {
-                return 8 + val[2] / 1000; //圆环大小
-            },
-            itemStyle: {
-                normal: {
-                    show: true,
-                },
-                emphasis: {
-                    show: true,
-                    color: "#FF6A6A"
-                }
-            },
-            data: item[1].map(function(dataItem) {
-                return {
-                    name: dataItem[0].name,
-                    value: geoCoordMap[dataItem[0].name].concat([dataItem[0].value])
-                };
-            })
+        type: "effectScatter",
+        coordinateSystem: "geo",
+        zlevel: 2,
+        rippleEffect: {
+            //涟漪特效
+            period: 4, //动画时间，值越小速度越快
+            brushType: "stroke", //波纹绘制方式 stroke, fill
+            scale: 4 //波纹圆环最大限制，值越大波纹越大
         },
-        {
-            type: "scatter",
-            coordinateSystem: "geo",
-            zlevel: 2,
-            rippleEffect: {
-                period: 4,
-                brushType: "stroke",
-                scale: 4
+        label: {
+            show: true,
+            position: "right", //显示位置
+            offset: [5, 0], //偏移设置
+            formatter: "{b}",//圆环显示文字
+            backgroundColor:"transparent" //设置label文字背景为空
+        },
+        symbol: "circle",
+        symbolSize: function(val) {
+            return 8 + val[2] / 1000; //圆环大小
+        },
+        itemStyle: {
+            show: true,
+            textShadowColor:"transparent"
+        },
+        emphasis: {
+            itemStyle:{
+                show: true,
+                color: "#FF6A6A"
             },
-            label: {
-                normal: {
-                    show: true,
-                    color: "red",
-                    position: "right",
-                    formatter: "{b}",
-                },
-                emphasis: {
-                    show: true,
-                    color: "#FF6A6A"
-                }
+            label:{
+                show: true,
+                color: "#FF6A6A"
+            }
+        },
+        data: item[1].map(function(dataItem) {
+            return {
+                name: dataItem[0].name,
+                value: geoCoordMap[dataItem[0].name].concat([dataItem[0].value])
+            };
+        })
+    },
+    {
+        type: "scatter",
+        coordinateSystem: "geo",
+        zlevel: 2,
+        rippleEffect: {
+            period: 4,
+            brushType: "stroke",
+            scale: 4
+        },
+        label: {
+            show: true,
+            color: "red",
+            position: "right",
+            formatter: "{b}",
+           
+        },
+        symbol: "pin",
+        symbolSize: 30,
+        itemStyle: {
+            show: true,
+            color: "red"
+        },
+        emphasis: {
+            label:{
+                show: true,
+                color: "#FF6A6A"
             },
-            symbol: "pin",
-            symbolSize: 30,
-            itemStyle: {
-                normal: {
-                    show: true,
-                    color: "red",
-                },
-                emphasis: {
-                    show: true,
-                    color: "#FF6A6A"
-                }
-            },
-            data: [{
-                name: item[0],
-                value: geoCoordMap[item[0]].concat([100]),
-                visualMap: false
-            }]
-        }
+            itemStyle:{
+                show: true,
+                color: "#FF6A6A"
+            }
+        },
+        data: [{
+            name: item[0],
+            value: geoCoordMap[item[0]].concat([100]),
+            visualMap: false
+        }]
+    }
     );
 });
 var svg="path://M32.597,9.782 L30.475,11.904 C30.085,12.294 29.452,12.294 29.061,11.904 C28.671,11.513 28.671,10.880 29.061,10.489 L31.182,8.368 C31.573,7.978 32.206,7.978 32.597,8.368 C32.987,8.759 32.987,9.392 32.597,9.782 ZM30.000,30.500 C30.000,31.328 29.329,32.000 28.500,32.000 L5.500,32.000 C4.672,32.000 4.000,31.328 4.000,30.500 C4.000,29.672 4.672,29.000 5.500,29.000 L8.009,29.000 L8.009,18.244 C8.009,13.139 12.034,9.000 17.000,9.000 C21.966,9.000 25.992,13.139 25.992,18.244 L25.992,29.000 L28.500,29.000 C29.329,29.000 30.000,29.672 30.000,30.500 ZM17.867,14.444 L13.000,22.000 L17.000,22.000 L17.133,26.556 L21.000,20.000 L17.000,20.000 L17.867,14.444 ZM25.221,6.327 C25.033,6.846 24.459,7.113 23.940,6.924 C23.421,6.735 23.153,6.162 23.342,5.643 L24.368,2.823 C24.557,2.304 25.131,2.037 25.650,2.226 C26.169,2.415 26.436,2.989 26.248,3.508 L25.221,6.327 ZM17.000,5.000 C16.448,5.000 16.000,4.552 16.000,4.000 L16.000,1.000 C16.000,0.448 16.448,0.000 17.000,0.000 C17.552,0.000 18.000,0.448 18.000,1.000 L18.000,4.000 C18.000,4.552 17.552,5.000 17.000,5.000 ZM10.028,7.197 C9.509,7.386 8.935,7.118 8.746,6.599 L7.720,3.780 C7.532,3.261 7.799,2.687 8.318,2.498 C8.837,2.309 9.411,2.577 9.600,3.096 L10.626,5.915 C10.815,6.434 10.547,7.008 10.028,7.197 ZM3.354,12.268 L1.232,10.146 C0.842,9.756 0.842,9.123 1.232,8.732 C1.623,8.342 2.256,8.342 2.646,8.732 L4.768,10.854 C5.158,11.244 5.158,11.877 4.768,12.268 C4.377,12.658 3.744,12.658 3.354,12.268 Z"
@@ -277,18 +275,18 @@ export const MapOption = {
         enterable: true,
         transitionDuration: 0,
         extraCssText: "z-index:100",
-        formatter: function(params, ticket, callback) {
-            //根据业务自己拓展要显示的内容
-            var res = "";
-            var name = params.name;
-            var value = params.value[params.seriesIndex + 1];
-            res =
-                "<span style='color:#fff;'>" +
-                name +
-                "</span><br/>数据：" +
-                value;
-            return res;
-        }
+        // formatter: function(params, ticket, callback) {
+        //     //根据业务自己拓展要显示的内容
+        //     var res = "";
+        //     var name = params.name;
+        //     var value = params.value[params.seriesIndex + 1];
+        //     res =
+        //         "<span style='color:#fff;'>" +
+        //         name +
+        //         "</span><br/>数据：" +
+        //         value;
+        //     return res;
+        // }
     },
     visualMap: {
         //图例值控制
@@ -313,38 +311,33 @@ export const MapOption = {
     geo: {
         map: "world",
         show: true,
-        label: {
-            emphasis: {
-                show: false
-            }
-        },
         roam: true, //是否允许缩放
         layoutCenter: ["50%", "50%"], //地图位置
-        layoutSize: "120%",
+        layoutSize: "185%",
         itemStyle: {
-            normal: {
-                show: 'true',
-                color: "#fff", //地图背景色
-                borderColor: "#5bc1c9" //省市边界线
-            },
-            emphasis: {
+            show: 'true',
+            color: "#fff", //地图背景色
+            borderColor: "#5bc1c9"//省市边界线
+        },
+        emphasis: {
+            itemStyle:{
                 show: 'true',
                 color: "rgba(37, 43, 61, .5)" //悬浮背景
+            },
+            label:{
+                show: false
             }
+           
         }
     },
     legend: {
-    	        orient: 'vertical',
-    	        top: '30',
-    	        left: 'center',
-    	        align:'right',
-    	        textStyle: {
-    	            color: '#fff',
-    	            fontSize:20,
-    	        },
-    	        itemWidth:50,
-    	        itemHeight:30,
-    	        selectedMode: 'multiple'
-    	    },
+        orient: 'vertical',
+        top: '30',
+        left: 'center',
+        align:'right',
+        itemWidth:50,
+        itemHeight:30,
+        selectedMode: 'multiple'
+    },
     series: series
 };
--- src/pages/data/index.js
+++ src/pages/data/index.js
@@ -9,17 +9,16 @@
  * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
  * See the Mulan PSL v2 for more details.
  */
- import React,{useState,useEffect} from 'react'
+
+ import React,{useEffect,useRef} from 'react'
+ import './worldmap'
  import './index.less';
  import { connect } from 'react-redux';
  import ReactEChartsCore from 'echarts-for-react/lib/core';
  import * as echarts from 'echarts/core';
+ import {CanvasRenderer} from 'echarts/renderers';
+ import {BarChart} from 'echarts/charts';
  import {
-    CanvasRenderer,
-    // SVGRenderer,
-  } from 'echarts/renderers';
-  import { BarChart} from 'echarts/charts';
-  import {
     LegendPlainComponent,
     GridComponent,
     DataZoomInsideComponent,
@@ -30,8 +29,9 @@
     TitleComponent,
     GeoComponent
   } from 'echarts/components';
-import option from '../../data/dataresult.json'
-import {MapOption} from '../../components/mapdata/index'
+import option from '../../data/dataresult.json'  //活动数据页面配置集合
+import {MapOption} from '../../components/mapdata/index' //地图配置
+
 echarts.use(
     [
         LegendPlainComponent,
@@ -48,128 +48,157 @@ echarts.use(
     ]
 );
 
-console.log(MapOption)
-function Data(){
-    const [options,setOptions] = useState(option)
+function Data(props){
+    let showdata = option[props.chiFlag]
+    const instance0 = useRef(null);
+    const instance1 = useRef(null);
+    const instance2 = useRef(null);
+    const instance3 = useRef(null);
+    const instance4 = useRef(null);
+    useEffect(()=>{
+        const myChart0 = instance0 && instance0.current.getEchartsInstance();
+        const myChart1 = instance1 && instance1.current.getEchartsInstance();
+        const myChart2 = instance2 && instance2.current.getEchartsInstance();
+        const myChart3 = instance3 && instance3.current.getEchartsInstance();
+        const myChart4 = instance4 && instance3.current.getEchartsInstance();
+        window.onresize = function(){
+            myChart0 && myChart0.resize();
+            myChart1 && myChart1.resize();
+            myChart2 && myChart2.resize();
+            myChart3 && myChart3.resize();
+            myChart4 && myChart4.resize();
+        }
+    },[])
     return(         
         <div className="datas">
             <picture className="datas_header">  
-            <source srcSet="../../img/index/banner3.png" media="(min-width: 800px)" />  
-            <source srcSet="medium.jpg" media="(min-width: 600px)" />  
-            <img srcSet="small.jpg" />  
-        </picture> 
-        <div className = "datas_content_show">
+                <source srcSet="../../img/index/banner3.png" media="(min-width: 800px)" />  
+                <source srcSet="medium.jpg" media="(min-width: 600px)" />  
+                <img srcSet="small.jpg" />  
+            </picture> 
+            <div className = "datas_content_show content1200">
 
-            {/* 项目数据 */}
-            <div className = "pro_data">
-                <div className = "pro_data_title">
-                    {option.pro_data.name}
+                {/* 项目数据 */}
+                <div className = "pro_data">
+                    <div className = "pro_data_title">
+                        {showdata.pro_data.name}
+                    </div>
+                    <div className = "pro_data_bar1">
+                        <ReactEChartsCore
+                            echarts={echarts}
+                            option={showdata.pro_data.bar_one.option}
+                            notMerge={false}
+                            lazyUpdate={false}
+                            ref={instance0}
+                            />
+                    </div>
+                    <div className="pro_data_divide"></div>
+                    <div className = "pro_data_bar2title">
+                        {showdata.pro_data.bar_two.title}
+                    </div>
+                    <div className = "pro_data_bar2">
+                        <ReactEChartsCore
+                            echarts={echarts}
+                            option={showdata.pro_data.bar_two.option}
+                            ref={instance1}
+                            />
+                    </div>
+                    <div className="pro_data_divide"></div>
+                    <div className = "pro_data_bar3title">
+                        {showdata.pro_data.bar_three.title}
+                    </div>
+                    <div className = "pro_data_bar3">
+                        <ReactEChartsCore
+                            echarts={echarts}
+                            option={showdata.pro_data.bar_three.option}
+                            ref={instance2}
+                            />
+                    </div>
                 </div>
-                <div className = "pro_data_bar1">
-                    <ReactEChartsCore
-                        echarts={echarts}
-                        option={options.pro_data.bar_one.option}
-                        notMerge={false}
-                        lazyUpdate={false}
-                        />
-                </div>
-                <div className="pro_data_divide"></div>
-                <div className = "pro_data_bar2title">
-                    {option.pro_data.bar_two.title}
-                </div>
-                <div className = "pro_data_bar2">
-                    <ReactEChartsCore
-                        echarts={echarts}
-                        option={options.pro_data.bar_two.option}
-                        />
-                </div>
-                <div className="pro_data_divide"></div>
-                <div className = "pro_data_bar3title">
-                    {option.pro_data.bar_three.title}
-                </div>
-                <div className = "pro_data_bar3">
-                    <ReactEChartsCore
-                        echarts={echarts}
-                        option={options.pro_data.bar_three.option}
-                        />
-                </div>
-            </div>
 
-             {/* 学生数据 */}
-             <div className ='stu_data'>
-                <div className = "stu_data_title">
-                        {option.stu_data.name}
-                </div>
-                <div className = "stu_data_des">
-                    {
-                        option.stu_data && option.stu_data.stu_data_des.map((ele,index) => {
-                            return (
-                                <div key={index} className = "stu_data_desItem">
-                                    <div className = "stu_data_desInfo">
-                                        <div className = 'stu_data_desItem_key'>
-                                            {Reflect.ownKeys(ele)[0]}
-                                        </div>
-                                        <div className = 'stu_data_desItem_val'>
-                                            {ele[Reflect.ownKeys(ele)[0]]}
-                                            <span className = "stu_data_unit">{index == 1? '次':'数'}</span> 
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }                
-                </div>
-                <div className = "stu_data_bar2_title">
-                    {option.stu_data.bar_two.title}
-                </div>
-                <div className = "stu_data_bar2">
-                    <ReactEChartsCore
-                        echarts={echarts}
-                        option={options.stu_data.bar_two.option}
-                    />
-                </div>
-             </div>
+                {/* 学生数据 */}
+                <div className ='stu_data'>
+                    <div className = "stu_data_title">
+                            {showdata.stu_data.name}
+                    </div>
+                    <div className = "stu_data_des">
+                        {
+                            showdata.stu_data && showdata.stu_data.stu_data_des.map((ele,index) => {
+                                return (
+                                    <div key={index} className = "stu_data_desItem">
+                                        <div className = "stu_data_desInfo">
+                                            <div className = 'stu_data_desItem_key'>
+                                                {Reflect.ownKeys(ele)[0]}
+                                            </div>
 
-             {/* 组织数据 */}
-             <div className = "org_data">
-                <div className = "org_data_title">
-                    {option.org_data.name}
-                </div>
-                <div className = "org_data_des">
-                    {
-                        option.org_data && option.org_data.org_data_des.map((ele,index) => {
-                            return (
-                                <div key={index} className = "org_data_desItem">
-                                    <div className = "org_data_desInfo">
-                                        <div className = 'org_data_desItem_key'>
-                                            {Reflect.ownKeys(ele)[0]}
-                                        </div>
-                                        <div className = 'org_data_desItem_val'>
-                                            {ele[Reflect.ownKeys(ele)[0]] + '个'}
+                                            {
+                                                <div className = 'stu_data_desItem_val'>
+                                                    {ele[Reflect.ownKeys(ele)[0]]}
+                                                    {props.chiFlag == 'chi'?<span className = "stu_data_unit">{index == 1? '次':'名'}</span>:''} 
+                                                </div>
+                                            }
                                         </div>
                                     </div>
-                                </div>
-                            )
-                        })
-                    }                
+                                )
+                            })
+                        }                
+                    </div>
+                    <div className = "stu_data_bar2_title">
+                        {showdata.stu_data.bar_two.title}
+                    </div>
+                    <div className = "stu_data_bar2">
+                        <ReactEChartsCore
+                            echarts={echarts}
+                            option={showdata.stu_data.bar_two.option}
+                            ref={instance3}
+                        />
+                    </div>
                 </div>
-                <div className = "org_data_bar1_title">
-                    {option.org_data.bar_one.title}
+
+                {/* 组织数据 */}
+                <div className = "org_data">
+                    <div className = "org_data_title">
+                        {showdata.org_data.name}
+                    </div>
+                    <div className = "org_data_des">
+                        {
+                            showdata.org_data && showdata.org_data.org_data_des.map((ele,index) => {
+                                return (
+                                    <div key={index} className = "org_data_desItem">
+                                        <div className = "org_data_desInfo">
+                                            <div className = 'org_data_desItem_key'>
+                                                {Reflect.ownKeys(ele)[0]}
+                                            </div>
+                                            <div className = 'org_data_desItem_val'>
+                                                {ele[Reflect.ownKeys(ele)[0]]}{props.chiFlag == 'chi'?'个':''}
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }                
+                    </div>
+                    <div className = "org_data_bar1_title">
+                        {showdata.org_data.bar_one.title}
+                    </div>
+                    <div className = "org_data_bar1">
+                        <div className = "org_map">
+                            <ReactEChartsCore
+                                echarts={echarts}
+                                option={MapOption}
+                                ref={instance4}
+                            />
+                        </div>
+                        <div className = "org_map_des">
+                            {
+                                showdata.org_data.bar_one.bar_des.map((item,index)=>{
+                                    return <div className = "org_map_desItem" key={index}>{Reflect.ownKeys(item)[0]+item[Reflect.ownKeys(item)[0]]} {props.chiFlag=='chi'?'所':''}</div>
+                                })
+                            }
+                        </div>
+                    </div>  
                 </div>
-                <div className = "org_data_bar1">
-                    {/* <ReactEChartsCore
-                        echarts={echarts}
-                        option={MapOption}
-                    /> */}
-                </div>  
-             </div>
-            
-
-
-
-
-        </div> 
-
+            </div> 
         </div>
     )
  }
@@ -179,6 +208,5 @@ function Data(){
          chiFlag:state.chiFlag
      }
   }
- 
  
  export default connect(mapStateToProps)(Data)
